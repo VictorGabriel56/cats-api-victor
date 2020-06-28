2d057b565348e0a32b6a6db2125bd8b44da0e2ce
Victor Gabriel <victor.gabriel@conductor.com.br>
2020-06-28 14:47:50 -0300

    add pino and fix log

--- routes/listAll.js
+++ routes/listAll.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const Breeds = require('../models/breeds')
+const Category = require('../models/categoryPics')
 const router = express.Router();
 const length = require('length');
 const { render } = require('../app');
 const pino = require('pino');
 // const logger = pino(pino.destination('./logs.log'))
 const log = pino(pino.destination('./logs/logs.log'), { level: process.env.LOG_LEVEL || 'info' });
+const logger = pino({ prettyPrint: { suppressFlushSyncWarning: true } });
 // const log = pino({ level: process.env.LOG_LEVEL || 'info' });
 const expressPino = require('express-pino-logger');  //modified
-const expressLogger = expressPino({ logger:log });  //added
+const expressLogger = expressPino({ logger: log });  //added
 router.use(expressLogger) //modified
 
 
@@ -16,31 +18,73 @@ router.use(expressLogger) //modified
 router.get('/all', async (req, res, next) => {
     const all = await Breeds.find();;
     return res.send(all);
-    res.status(400).send();
 });
 
 //listar somente raças
 router.get('/all/breeds', async (req, res, next) => {
+
     const all = await Breeds.find({}, { "Breed": 1 });;
-    return res.send(all);
+
+    return res.send(all.data);
 });
 
 //listar info de raças pelo nome da raça
 router.get('/breed/:infoId', async (req, res, next) => {
-    const all = await Breeds.find({Breed: req.params.infoId});
-    return res.send(all);
+    var all = await Breeds.findOne({ Breed: req.params.infoId });
+
+    if (all != null) {
+        logger.info('Raca encontrada!')
+        return res.send(all);
+    } else {
+        logger.info("Log: Raca nao encontrada ao listar.")
+        res.status(404).send({
+            mensagem: 'message: Essa raca não foi encontrada.'
+        });
+    }
 });
 
 //listar raças de acordo com o temperamento
 router.get('/temp/:infoTemp', async (req, res, next) => {
-    const all1 = await Breeds.find({"Temperament": {'$regex': req.params.infoTemp, '$options' : 'i'}});
-    return res.send(all1);
+    var all = await Breeds.find({ "Temperament": { '$regex': req.params.infoTemp, '$options': 'i' } });
+
+
+    if (all.length != 0) {
+        logger.info('Raca(s) encontrada(s) de acordo com o temperamento informado.')
+        return res.send(all);
+    } else {
+        logger.info("Log: Raca nao encontrada ao listar utilizando temperamento informado.")
+        res.status(404).send({
+            mensagem: 'message: Raca nao encontrada ao listar utilizando temperamento informado'
+        });
+    }
 });
 
 //listar raças de acordo com a origem
 router.get('/origin/:infoOrigin', async (req, res, next) => {
-    const all1 = await Breeds.find({"Origin": {'$regex': req.params.infoOrigin, '$options' : 'i'}});
-    return res.send(all1);
+    const all = await Breeds.find({ "Origin": { '$regex': req.params.infoOrigin, '$options': 'i' } });
+
+    if (all.length != 0) {
+        logger.info('Raca(s) encontrada(s) de acordo com o temperamento informado.')
+        return res.send(all);
+    } else {
+        logger.info("Log: Raca(s) nao encontrada(s) ao listar utilizando temperamento passado.")
+        res.status(404).send({
+            mensagem: 'message: Raca nao encontrada ao listar de acordo com a origem informada'
+        });
+    }
+});
+//listar gatos de chapéu
+router.get('/all/hat', async (req, res, next) => {
+    const all = await Category.find({ Category: 'Hat' });;
+    logger.info("Fotos de gato de chapeu listadas.")
+    return res.send(all);
+});
+
+//listar gatos de oculos
+router.get('/all/sunglass', async (req, res, next) => {
+    const all = await Category.find({ Category: 'Sunglasses' });
+    logger.info("Fotos de gato de oculos listadas.")
+    return res.send(all);
 });
 
 module.exports = app => app.use('/list', router);
